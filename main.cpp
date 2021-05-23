ca0509d965df8c0739a031d61507d0002b43b00a
Jakub Zieliński <j.zielinski@stud.elka.pw.edu.pl>
2021-05-23 13:20:19 +0200

    c++ | decision node

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ int main() {
     DecisionNode root(1, -1, nullptr);
 
 
-    root.children[0] = new DecisionNode(2, 0 , nullptr);
-    root.children[1] = new DecisionNode(2, 1 , nullptr);
-    root.children[2] = new DecisionNode(2, 2 , nullptr);
-    root.children[3] = new DecisionNode(2, 3 , nullptr);
-    root.children[4] = new DecisionNode(2, 4 , nullptr);
+    root.children[0] = new DecisionNode(2, 0 , &root);
+    root.children[1] = new DecisionNode(2, 1 , &root);
+    root.children[2] = new DecisionNode(2, 2 , &root);
+    root.children[3] = new DecisionNode(2, 3 , &root);
+    root.children[4] = new DecisionNode(2, 4 , &root);
 
 
 
