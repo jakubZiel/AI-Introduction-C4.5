c1c157aa576495bf1bd93f14a274a36f9f37f96b
LWronska <laura.wronska12@gmail.com>
2021-05-27 21:44:00 +0200

    trying to display tree using treelibrary

--- id3.py
+++ id3.py
@@ -1,5 +1,6 @@
 import math
 import DecisionNode
+from treelib import Tree, Node
 
 def mostFreqClass(dataset):
     valueFreq = {}
@@ -136,10 +137,73 @@ def ID3(attributes, dataset, recursionLevel, parentNode):
 
     return Tree
 
+def Show_ID3(attributes, dataset, recursionLevel, parentNode):
+    recursionLevel += 1
+
+    #same class in all cases, put it into leaf
+    #prepare classes from current dataset
+    classes =[]
+    for row in dataset:
+        #        print(row)
+        classes.append(row[-1])
+
+    #    print(classes)
+    if classes.count(classes[0]) == len(classes):
+        c = classes[0]
+        #return DecisionNode.DecisionNode(None, c, parentNode)
+        leaf = Tree()
+        leaf.create_node(c, c, parentNode)
+        leaf.show()
+        return leaf
+
+    #no more attributes -> get most frequent class
+    if (len(attributes) - 1) <= 0:
+        mfc = mostFreqClass(dataset)
+        #return DecisionNode.DecisionNode(None, mfc, parentNode)
+        leaf = Tree()
+        leaf.create_node(mfc, mfc, parentNode)
+        leaf.show()
+        return leaf
+
+    #d - attribute with highest InfGain
+    d = bestAttribute(attributes, dataset)
+    test_tree = Tree()
+    test_tree.create_node(d, d, parentNode)
+    test_tree.show()
+    #Tree = DecisionNode.DecisionNode(d, None, parentNode)
+
+    #preparing attribute parameters for children nodes
+    attrIndex = d
+    del(attributes[d])
+
+    attrValues = [row[d] for row in dataset]
+    attrValues = set(attrValues)
+
+    # well tested until here
+
+    #children nodes
+    for attrVal in attrValues:
+        subAttr = attributes[:]
+        test_tree.create_node(attrVal, attrVal, parent=d)
+        test_tree.show()
+
+        #tu sie psuje
+        test_tree.paste(attrVal, Show_ID3(subAttr, split_data(attrIndex, attrVal, dataset), recursionLevel, attrVal))
+        test_tree.show()
+        #test_tree.subtree(attrVal).paste(attrVal, Show_ID3(subAttr, split_data(attrIndex, attrVal, dataset), recursionLevel, test_tree))
+
+        #Tree.children[attrVal] = ID3(subAttr, split_data(attrIndex, attrVal, dataset), recursionLevel, Tree)
+
+    return test_tree
+
+
 def test1():
 
     dataset = [[2, 1, 3, 4], [1, 1, 2, 4], [1, 3, 3, 4], [4, 3, 4, 0]]
     dataset2 = [[1,1,0], [2,1,1], [2,2,1], [2,2,0], [2,3,1]]
     attr = [0, 1]
 
-    tree = ID3(attr, dataset2, 0, None)
+    #tree = ID3(attr, dataset2, 0, None)
+
+    tree = Show_ID3(attr, dataset2, 0, None)
+    tree.show()
