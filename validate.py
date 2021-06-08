56c715078a94c75979d6f603cb0e2d3cc9dc4b0b
Jakub Zieliński <j.zielinski@stud.elka.pw.edu.pl>
2021-06-08 21:24:27 +0200

    bierdra changes

--- validate.py
+++ validate.py
@@ -5,7 +5,7 @@ def aggregateResults():
     print("No cross validation, entire dataset:")
 
     print("With cross validation:")
-    for i in range(10, 4, -1):
+    for i in range(10, 5, -1):
         print("K = ", i)
         testMore(i, ID3, 10)
         testMore(i, C45, 10)
