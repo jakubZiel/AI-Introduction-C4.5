7b384e80854a40692ad0f8cefb34ec2bf77ff25d
LWronska <laura.wronska12@gmail.com>
2021-06-09 12:40:54 +0200

    tests aggregation with commercial tree

--- CommercialTree.py
+++ CommercialTree.py
@@ -1,20 +1,36 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import confusion_matrix
 
-dataset = pd.read_csv("data/divorce2.csv")
 
-X = dataset.drop('Class', axis=1)
-Y = dataset['Class']
+def commercial():
+    dataset = pd.read_csv("data/divorce2.csv")
 
-for i in range(5):
+    X = dataset.drop('Class', axis=1)
+    Y = dataset['Class']
+
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
 
     classifier = DecisionTreeClassifier()
     classifier.fit(X_train, y_train)
 
     y_pred = classifier.predict(X_test)
-    print(confusion_matrix(y_test, y_pred))
+    cm = confusion_matrix(y_test, y_pred)
+    return cm
 
-    print(classification_report(y_test, y_pred))
+def avg_commercial_error(testNumber: int):
+
+    avgError = 0.0
+
+    for i in range (testNumber):
+        conf_matrix = commercial()
+        successNumber = conf_matrix[0][0] + conf_matrix[1][1]
+        tryNumber = successNumber + conf_matrix[0][1] + conf_matrix[1][0]
+
+        avgError = avgError + (1-successNumber/tryNumber)
+
+    avgError = avgError/testNumber
+    avgError = float("{:.3f}".format(avgError))
+    print("avg error : " + str(avgError))
+
--- validate.py
+++ validate.py
@@ -1,14 +1,18 @@
 from utilities.Validation import *
+from CommercialTree import *
 
 
 def aggregateResults():
-    print("No cross validation, entire dataset:")
 
-    print("With cross validation:")
-    for i in range(10, 5, -1):
-        print("K = ", i)
-        testMore(i, ID3, 5)
-        testMore(i, C45, 5)
+    print ("Our own implementation.")
+    print("Cross validation. K = 10")
+    K = 10
+    testNumber = 10     #number of tests to aggregate
+    testMore(K, ID3, 10)
+    testMore(K, C45, 10)
+
+    print("Commercial implementation.")
+    avg_commercial_error(testNumber)
 
 
 aggregateResults()
