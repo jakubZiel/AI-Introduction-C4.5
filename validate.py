a81c4908fa736d068508c77d00106e90008cd73b
Jakub Zieliński <j.zielinski@stud.elka.pw.edu.pl>
2021-06-09 08:07:43 +0200

    commercial implementation

--- validate.py
+++ validate.py
@@ -7,8 +7,8 @@ def aggregateResults():
     print("With cross validation:")
     for i in range(10, 5, -1):
         print("K = ", i)
-        testMore(i, ID3, 10)
-        testMore(i, C45, 10)
+        testMore(i, ID3, 5)
+        testMore(i, C45, 5)
 
 
 aggregateResults()
